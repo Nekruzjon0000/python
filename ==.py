85d8afea0a0fcf14475f742fd2dd6124571361d5
Nekruzjon0000 <146971714+Nekruzjon0000@users.noreply.github.com>
2023-10-04 11:31:03 -0400

    Add files via upload

---
+++ ==.py
@@ -0,0 +1,5 @@
+x, y, z = int(input()), int(input()), int(input()),
+if x == y == z:
+    print('число равни')
+else:
+    print('ощибка, числа не равны.')
---
+++ test.py
@@ -0,0 +1,8 @@
+
+answer = input('какой язык програмировние изучаем?')
+if answer == 'python':
+    print('верно мы учим Python')
+    print('python-отлисный язык')
+else:
+    print('это не так')
+print('программа выполнена')
